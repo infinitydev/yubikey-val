3b00cd45889c00f7ecfce9cf7c9f133e69896b69
Samir Patankar <samir@yubico.com>
2009-04-30 22:34:07 +0000

    Creating "RADIUS_on_Premise" branch for Two Factor Authentication using FreeRADIUS and Yubico YMS.

--- common.php
+++ common.php
@@ -1,72 +1,56 @@
 <?php
-
 define('S_OK', 'OK');
 define('S_BAD_OTP', 'BAD_OTP');
+define('S_BAD_CLIENT', 'BAD_CLIENT'); // New, added by paul 20080920
 define('S_REPLAYED_OTP', 'REPLAYED_OTP');
-define('S_DELAYED_OTP', 'DELAYED_OTP');
 define('S_BAD_SIGNATURE', 'BAD_SIGNATURE');
 define('S_MISSING_PARAMETER', 'MISSING_PARAMETER');
-define('S_NO_SUCH_CLIENT', 'NO_SUCH_CLIENT');
+//define('S_NO_SUCH_CLIENT', 'NO_SUCH_CLIENT'); // Deprecated by paul 20080920
 define('S_OPERATION_NOT_ALLOWED', 'OPERATION_NOT_ALLOWED');
 define('S_BACKEND_ERROR', 'BACKEND_ERROR');
+define('S_SECURITY_ERROR', 'SECURITY_ERROR');
+define('TS_SEC', 0.119);
+define('TS_TOLERANCE', 0.3);
+define('S_BAD_USERKEY', 'BAD_USER_TOKEN'); // New, added by networkmarvels 20081217
+define('S_NO_USERMAP', 'NO_USERKEY_MAP'); // New, added by networkmarvels 20081217
+define('S_BAD_USERSETTING', 'BAD_USER_SETTING'); // New, added by networkmarvels 20081217
 
-define('TS_SEC', 1/8);
-define('TS_REL_TOLERANCE', 0.3);
-define('TS_ABS_TOLERANCE', 20);
-
-define('TOKEN_LEN', 32);
-
-function unescape($s) {
-	return str_replace('\\', "", $s);
+function debug($msg, $exit = false) {
+	global $trace;
+	if ($trace) {
+		if (is_array($msg)) {
+			//print_r($msg);
+		} else {
+			echo '<p>Debug> ' . $msg;
+		}
+		echo "\n";
+	}
+	if ($exit) {
+		die('<font color=red><h4>Exit</h4></font>');
+	}
 }
 
-function getHttpVal($key, $defaultVal) {
-	$val = $defaultVal;
-	if (array_key_exists($key, $_GET)) {
-		$val = $_GET[$key];
-  	} else if (array_key_exists($key, $_POST)) {
-  		$val = $_POST[$key];
-  	}
-  	$v = unescape(trim($val));
-  	return $v;
+function genRandRaw($len) {
+	$h = hash_hmac('sha1', rand(9999,9999999), 'dj*ccbcuiiurubrvnubcdluul', true);
+	$a = str_split($h);
+	//print_r($a);
+	$a = array_slice($a, 0, $len);
+	//print_r($a);
+	$s = implode($a);
+	//outputToFile('out', $s);
+	return $s;
 }
 
-function query($conn, $q) {
-  debug('Query: '.$q);
-  $result = mysql_query($q, $conn);
-  if (!$result) {
-    die("Query error: " . mysql_error());
-  }
-  return $result;
-}
-
-function mysql_quote($value) {
-	return "'" . mysql_real_escape_string($value) . "'";	
-}
-
-function debug($msg) {
-  if (is_array($msg)) {
-    $str = "";
-    foreach($msg as $key => $value){
-      $str .= "$key=$value ";
-    }
-  } else {
-    $str = $msg;
-  }
-  error_log($str);
-}
-
-// Return eg. 2008-11-21T06:11:55Z0711
+// Return eg. 2008-11-21T06:11:55
 //            
 function getUTCTimeStamp() {
 	date_default_timezone_set('UTC');
-	$tiny = substr(microtime(false), 2, 3);
-	return date('Y-m-d\TH:i:s\Z0', time()) . $tiny;
+	return date('Y-m-d\TH:i:s', time());
 }
 
 // Sign a http query string in the array of key-value pairs
 // return b64 encoded hmac hash
-function sign($a, $apiKey) {
+function sign($a, $apiKey, $debug=false) {
 	ksort($a);
 	$qs = '';
 	$n = count($a);
@@ -78,79 +62,23 @@ function sign($a, $apiKey) {
 		}
 	}
 	
+	// Generate the signature
+//	debug('API key: '.base64_encode($apiKey)); // API key of the client
+	debug('SIGN: '.$qs);
+	
 	// the TRUE at the end states we want the raw value, not hexadecimal form
 	$hmac = hash_hmac('sha1', utf8_encode($qs), $apiKey, true);
 	$hmac = base64_encode($hmac);
-
-	debug('SIGN: ' . $qs . ' H=' . $hmac);
-
+	if ($debug) {
+		debug('h='.$hmac);		
+	}
 	return $hmac;
 		
 } // sign an array of query string
 
-function hex2b64 ($hex_str) {
-  $bin = pack("H*", $hex_str);
-  return base64_encode($bin);
+function outputToFile($outFname, $content, $mode, $append = false) {
+	$out = fopen($outFname, ($append ? "a" : "w"));
+	fwrite($out, $content);
+	fclose($out);
 }
-
-function modhex2b64 ($modhex_str) {
-  $hex_str = strtr ($modhex_str, "cbdefghijklnrtuv", "0123456789abcdef");
-  return hex2b64($hex_str);
-}
-
-// $otp: A yubikey OTP
-function decryptOTP($otp, $base_url) {
-  $url = $base_url . $otp;
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_USERAGENT, "YK-VAL");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_FAILONERROR, true);
-  curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-  $response = curl_exec($ch);
-  $error = curl_error ($ch);
-  $errno = curl_errno ($ch);
-  debug("YK-KSM response: $response errno: " . $errno . " error: " . $error);
-  $info = curl_getinfo ($ch);
-  debug($info);
-  curl_close($ch);
-
-  if (sscanf ($response,
-	      "OK counter=%04x low=%04x high=%02x use=%02x",
-	      $ret["session_counter"], $ret["low"], $ret["high"],
-	      $ret["session_use"]) != 4) {
-    return false;
-  }
-  return $ret;
-} // End decryptOTP
-
-// $devId: The first 12 chars from the OTP
-function getAuthData($conn, $devId) {
-	$tokenId = modhex2b64($devId);
-	$stmt =
-	  'SELECT id, client_id, counter, sessionUse, low, high, accessed '.
-	  'FROM yubikeys '.
-	  'WHERE active AND tokenId='.mysql_quote($tokenId);
-	$r = query($conn, $stmt);
-	if (mysql_num_rows($r) > 0) {
-		$row = mysql_fetch_assoc($r);
-		mysql_free_result($r);
-		return $row;
-	}
-	return null;
-} // End getAuthData
-
-// $clientId: The decimal client identity
-function getClientData($conn, $clientId) {
-	$stmt =
-	  'SELECT id, secret, chk_time '.
-	  'FROM clients '.
-	  'WHERE active AND id='.mysql_quote($clientId);
-	$r = query($conn, $stmt);
-	if (mysql_num_rows($r) > 0) {
-		$row = mysql_fetch_assoc($r);
-		mysql_free_result($r);
-		return $row;
-	}
-	return null;
-} // End getClientData
 ?>
